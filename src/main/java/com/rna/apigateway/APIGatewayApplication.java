30e8aef8af125b2ad7b9e17d4df87a9a5388286c
Rahul Agrawal <rahulagrawal.sw@gmail.com>
2020-03-02 20:25:09 +0530

    Comments added to main class

--- src/main/java/com/rna/apigateway/APIGatewayApplication.java
+++ src/main/java/com/rna/apigateway/APIGatewayApplication.java
@@ -10,6 +10,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
+/*
+* API Gateway - using ZUUL and Ribbon
+*/
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
